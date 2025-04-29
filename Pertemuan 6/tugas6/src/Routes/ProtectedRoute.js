683f95f840d0caac707809b320198fc91f140fb8
Eva Fadhillah Ulia <115793590+evafadhillahulia@users.noreply.github.com>
2025-04-29 10:15:54 +0700

    Add files via upload

---
+++ Pertemuan 6/tugas6/src/Routes/ProtectedRoute.js
@@ -0,0 +1,17 @@
+// src/Routes/ProtectedRoute.js
+
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+  // ✅ Di sinilah <Navigate /> digunakan:
+  if (!isLoggedIn) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
